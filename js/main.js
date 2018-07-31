15e5f40b4f1e44535be859e52f2481ca4e7f84be
Blake <blakecapella9@gmail.com>
2018-07-30 20:02:40 -0400

    started filling

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Blake Capella", "Engineer.", "Developer.", "Writer.", "Leader."],
       typeSpeed: 100,
       loop: true,
     });
